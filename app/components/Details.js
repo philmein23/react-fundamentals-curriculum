77dfceee8213fc715f0e339c49b033c9ac8fe7eb
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-08-01 00:59:43 -0700

    added date in detail component

--- app/components/Details.js
+++ app/components/Details.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var convertUnixDate = require('../util/util');
 
 var styles = {
     container: {
@@ -22,17 +23,24 @@ var styles = {
 
 function Details(props) {
     var icon = props.weather.weather[0].icon;
+    var date = convertUnixDate(props.weather.dt);
 	return (
 		<div style={styles.container}>
             <h1 style={styles.header}>{props.city}</h1>
+            <div style={styles.subheader}>{date}</div>
             <img style={styles.icon} src={'/app/images/weather-icons/' + icon + '.svg'} />
             <h1 style={styles.header}>{props.weather.weather[0].description}</h1>
             <div style={styles.subheader}>Min temp: {props.weather.temp.min}</div>
-            <div style={styles.subheader}>Min temp: {props.weather.temp.max}</div>
+            <div style={styles.subheader}>Max temp: {props.weather.temp.max}</div>
             <div style={styles.subheader}>Humidity: {props.weather.humidity}</div>
         </div>
         )
 
 };
+
+Details.propTypes = {
+    city: PropTypes.string.isRequired,
+    weather: PropTypes.object.isRequired
+}
 
 module.exports = Details;
